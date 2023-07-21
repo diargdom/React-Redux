de621da6aa6c152f5599d3a5fad45eb68b03b9e6
Diego Argumedo <diegoargumedo.jcol@orange.com>
2023-07-21 01:57:44 -0500

    Feat: se implementa middlewares

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import Searcher from './components/Searcher';
+import PokemonList from './components/PokemonList';
+import {Col} from 'antd'
+import { useEffect } from 'react';
+import { getPokemon } from './api';
+import { setPokemons} from './actions';
+import logo from './statics/logo.svg'
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+
 
 function App() {
+
+  const pokemons = useSelector(state => state.pokemons)
+  const dispacth = useDispatch()
+
+  useEffect(() => {        
+    const fetchPokemon = async () => {
+      const pokemonsRes = await getPokemon()
+      dispacth(setPokemons(pokemonsRes))
+    }
+    fetchPokemon()
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Col span={4} offset={10}>
+      <img src={logo} alt='Pokedux' /> 
+    </Col>
+    <Col span={8} offset={8}>
+      <Searcher />
+      </Col>
+      <PokemonList pokemons={pokemons} />
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/Searcher.jsx
@@ -0,0 +1,7 @@
+import {Input} from 'antd'
+
+const Searcher = () => {
+    return <Input.Search placeholder='Busar...' />
+}
+
+export default Searcher;
